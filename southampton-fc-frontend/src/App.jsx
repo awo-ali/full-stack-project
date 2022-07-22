a5ba93aeaf90baf8e9f19c53ac47f5a7d91c46df
awo-ali <awoali@hotmail.co.uk>
2022-07-22 13:06:27 +0100

    setup and routing

--- southampton-fc-frontend/src/App.jsx
+++ southampton-fc-frontend/src/App.jsx
@@ -1,14 +1,23 @@
 
 import './App.scss';
 import Home from './container/Home/Home';
+import PlayerCards from './container/PlayerCards/PlayerCards';
+import SeasonsStats from './container/SeasonStats/SeasonStats';
 import Navbar from './container/Navbar/Navbar';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 const App = () => {
   return (
+    <Router>
     <div className="app">
     <Navbar/>
-    <Home/>
+    <Routes>
+      <Route path="/" element={<Home/>} />
+      <Route path="/playerCards" element={<PlayerCards/>} />
+      <Route path="/seasonStats" element={<SeasonsStats/>} />
+    </Routes>
     </div>
+    </Router>
   );
 }
 
