4d98fb2295869d8c95199500182bbc0946e3a9e5
awo-ali <awoali@hotmail.co.uk>
2022-07-22 13:06:53 +0100

    created menu page

--- southampton-fc-frontend/src/component/Menu/Menu.jsx
+++ southampton-fc-frontend/src/component/Menu/Menu.jsx
@@ -1,22 +1,29 @@
+import "./Menu.scss";
+import blackCross from "../../assets/images/black-cross.png";
+import { Link } from "react-router-dom";
 
-import './Menu.scss';
-import blackCross from"../../assets/images/black-cross.png";
-
-
-const Menu = ({toggleMenuContent}) => {
-  
-  
+const Menu = ({ toggleMenuContent}) => {
   return (
     <div className="menu">
-        <img className='menu__cross' src={blackCross} alt="Exit" onClick={toggleMenuContent}  />
-        <div className='menu__content'>
-            <h1 className='menu__home'>Home</h1>
-            <h2 className='menu__playerCards'>Player Cards</h2>
-            <h3 className='menu__seasonStats'>Season Stats</h3>
-        </div>
-   
+      <img
+        className="menu__cross"
+        src={blackCross}
+        alt="Exit"
+        onClick={toggleMenuContent}
+      />
+      <div className="menu__content">
+        <Link to="/">
+          <h1 className="menu__home">Home</h1>
+        </Link>
+        <Link to="/playerCards">
+          <h2 className="menu__playerCards">Player Cards</h2>
+        </Link>
+        <Link to="seasonStats">
+          <h3 className="menu__seasonStats">Season Stats</h3>
+        </Link>
+      </div>
     </div>
   );
-}
+};
 
 export default Menu;
