bf5a1b3bb191ae12c4d272f3800a3255999d8a28
awo-ali <awoali@hotmail.co.uk>
2022-07-22 13:07:30 +0100

    styled and added navbar

--- southampton-fc-frontend/src/container/Navbar/Navbar.jsx
+++ southampton-fc-frontend/src/container/Navbar/Navbar.jsx
@@ -1,25 +1,36 @@
-import React, { useState } from 'react';
-import './Navbar.scss'
-import logo from '../../assets/images/southampton-logo.png';
-import menu from '../../assets/images/menu-image.png';
-import Menu from '../../component/Menu/Menu';
+import React, { useState } from "react";
+import "./Navbar.scss";
+import logo from "../../assets/images/southampton-logo.png";
+import menu from "../../assets/images/menu-image.png";
+import Menu from "../../component/Menu/Menu";
 
 const Navbar = () => {
-
   const [toggleMenu, setToggleMenu] = useState(false);
 
   const toggleMenuContent = () => {
     setToggleMenu(!toggleMenu);
   };
-    return (
-      <div className="navbar">
+  return (
+    <div className="navbar">
       <img className="navbar__logo" src={logo} alt="southampton logo" />
       <h1>Southampton-FC</h1>
-      <img className='navbar__menu' onClick={toggleMenuContent} src={menu} alt="menu-icon" />
-      {toggleMenu && <Menu className="navbar_toggle" toggleMenuContent={toggleMenuContent} />}  
-      </div>
-    );
-  }
-  
-  export default Navbar;
-  
+      <img
+        className="navbar__menu"
+        onClick={toggleMenuContent}
+        src={menu}
+        alt="menu-icon"
+      />
+      {toggleMenu && (
+        <Menu
+          className="navbar_toggle"
+          toggleMenuContent={toggleMenuContent}
+          // link1="/home"
+          // link2="/playerCards"
+          // link3="/seasonStats"
+        />
+      )}
+    </div>
+  );
+};
+
+export default Navbar;
