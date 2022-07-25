9c88230585c1cb2ee99a65177b47fa09155fdcd9
awo-ali <awoali@hotmail.co.uk>
2022-07-25 10:15:03 +0100

    player stats card to go on the back of player card

--- southampton-fc-frontend/src/component/PlayerStats/PlayerStats.jsx
+++ southampton-fc-frontend/src/component/PlayerStats/PlayerStats.jsx
@@ -1,10 +1,17 @@
 
 import './PlayerStats.scss';
 
-const Home = () => {
+const PlayerStats = () => {
   return (
     <div className="PlayerStats">
-   <h1>Home page</h1>
+      <h1 className='PlayerStats__name'></h1>
+      <h2>Nationality </h2>
+      <h3>Date of Birth</h3>
+      <h4>Height</h4>
+      <h5>Appearances</h5>
+      <h6>Goals</h6>
+      <h7>Assists</h7>
+
     </div>
   );
 }
